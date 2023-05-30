c40cc877f1ef65275442b78d63c58c9bbaf78639
Lamorna Read <lamornaread@outlook.com>
2023-05-30 09:02:13 +0100

    slider banner

---
+++ js/banner.js
@@ -0,0 +1,14 @@
+$('.banner').slick({
+    arrows: false,
+    autoplay: true,
+    autoplayspeed: 3000,
+    dots: true,
+    // asNavfor: '.dots',
+});
+
+// $('.dots').slick({
+//     slidesToShow: 7,
+//     slidesToScroll: 1,
+//     asNavFor: '.banner',
+//     focusOnSelect: true
+//   });
