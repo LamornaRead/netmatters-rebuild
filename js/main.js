9b96c1140c996b0e17958b8d33cb4a6279c5d588
Lamorna Read <lamornaread@outlook.com>
2023-05-31 15:34:14 +0100

    sticky header
    sticky header and started adding cookie

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 //hamburger vars
+
 const hamburger = document.querySelector('.hamburger');
 const navSmall = document.querySelector('.side-nav');
 const body = document.querySelector('.container-main');
 const navLink = document.querySelectorAll('.nav-li');
+
+
+
+//sticky header vars
+
+let didScroll;
+let lastScrollTop = 0;
+let delta = 10; /* adjusts amount of scroll before active */
+const navbarHeight = $('.header').outerHeight();
+
+
+
 
 //hamburger event and function
 
@@ -21,3 +34,37 @@ function closeMenu() {
     navSmall.classList.remove('active');
     body.classList.remove('active');
 }
+
+
+
+//sticky header 
+
+$(window).scroll(function(event) {
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    const st =$(this).scrollTop();
+
+    if(Math.abs(lastScrollTop - st) <= delta) 
+        return;
+
+    if(st > lastScrollTop ) {
+        $('.header').addClass('nav-up');
+    } else {
+        if(st + $(window).height() < $(document).height()){
+            $('.header').removeClass('nav-up');
+            $('.header').addClass('sticky');
+            $('.banner').addClass('stuck');
+        }
+    }
+    lastScrollTop = st;
+}
+
