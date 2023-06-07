f16c51c8cfd139d0ed05a8b35652287b1575472a
Lamorna Read <lamornaread@outlook.com>
2023-06-07 16:37:23 +0100

    second reflection complete

--- js/banner.js
+++ js/banner.js
@@ -3,12 +3,5 @@ $('.banner').slick({
     autoplay: true,
     autoplayspeed: 3000,
     dots: true,
-    // asNavfor: '.dots',
-});
 
-// $('.dots').slick({
-//     slidesToShow: 7,
-//     slidesToScroll: 1,
-//     asNavFor: '.banner',
-//     focusOnSelect: true
-//   });
+});
